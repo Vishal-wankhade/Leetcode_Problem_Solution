ac490dcda704fde73fe046db4401ffa6a87a02c9
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-05 23:55:16 +0530

    Time: 73 ms (50.59%) | Memory: 58 MB (24.24%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+//  
+var sum = 0;
+var max =-Infinity;
+var n = nums.length;
+
+for(var i = 0; i < n; i++){
+   
+   sum += nums[i];
+
+  if(sum > max){
+      max = sum;
+  }
+   if(sum < 0){
+       sum = 0;
+   }
+
+  
+
+
+
+}
+
+return max;
+  
+};
