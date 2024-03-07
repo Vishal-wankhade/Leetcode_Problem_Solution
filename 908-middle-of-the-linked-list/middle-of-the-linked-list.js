3f22e6b39ed6996ee27196b1049e3b554f763ded
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-07 17:31:50 +0530

    Time: 50 ms (65.74%) | Memory: 48.8 MB (46.26%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+
+    var count = 0;
+
+    var temp = head;
+
+    while(temp.next !== null){
+        temp = temp.next;
+        count++;
+    }
+
+    var n = (count/2);
+
+   temp = head;
+   for(var i = 0; i < n; i++){
+       temp = temp.next;
+   }
+
+    return temp;
+    
+
+};
