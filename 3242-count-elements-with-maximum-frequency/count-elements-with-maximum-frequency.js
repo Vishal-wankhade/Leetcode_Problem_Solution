83a009317bed03088b10859228682b9f8cc4bee6
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-09 04:19:04 +0530

    Time: 71 ms (19.34%) | Memory: 51 MB (46.98%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
@@ -3,25 +3,25 @@
  * @return {number}
  */
 var maxFrequencyElements = function(nums) {
-    
-  const freq = new Map();  // Map to store frequency of elements
-    let maxFreq = 0;  // Variable to store maximum frequency
-    
-    // Count frequency of each element
-    for (let num of nums) {
-        freq.set(num, (freq.get(num) || 0) + 1);
-        maxFreq = Math.max(maxFreq, freq.get(num));
-    }
-    
-    let countMaxFreq = 0; // Count of elements with maximum frequency
-    
-    // Count elements with maximum frequency
-    for (let [key, value] of freq) {
-        if (value === maxFreq) {
-             countMaxFreq += value; 
-        }
-    }
-    
-    return countMaxFreq;
 
+    var map =new Map();
+    let max = 0;
+
+    for(var num of nums){
+       map.set(num , (map.get(num) || 0) + 1 );
+       max = Math.max(max, map.get(num));
+
+    }
+
+    count = 0;
+
+   for(var [key , value] of map){
+       if(value === max){
+           count += value;
+       }
+   }
+return count;
+
+    
+    
 };
