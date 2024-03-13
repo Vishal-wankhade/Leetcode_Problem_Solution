bfa38c528676f24b0df56321ec37c7e96c173c5f
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-13 18:41:09 +0530

    Time: 57 ms (61.54%) | Memory: 49.5 MB (20.51%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.js
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.js
@@ -3,22 +3,47 @@
  * @return {number}
  */
 var pivotInteger = function(n) {
-    var totalSum = (n * (n + 1)) / 2; // Calculate the sum of all integers from 1 to n
-    var leftSum = 1;
+    // var totalSum = (n * (n + 1)) / 2; // Calculate the sum of all integers from 1 to n
+    // var leftSum = 1;
 
     if(n == 1)
     return 1;
 
-    for (var x = 2; x < n; x++) {
-        leftSum += x; // Incrementally add x to the left sum
-        var rightSum = totalSum - leftSum + x;
+    // for (var x = 2; x < n; x++) {
+    //     leftSum += x; // Incrementally add x to the left sum
+    //     var rightSum = totalSum - leftSum + x;
         
      
 
-        if (leftSum === rightSum) {
-            return x; // Return the pivot integer x if left sum equals right sum
-        }
-    }
+    //     if (leftSum === rightSum) {
+    //         return x; // Return the pivot integer x if left sum equals right sum
+    //     }
+    // }
 
-    return -1; 
+    // return -1; 
+
+   var leftValue = 1;
+   var leftSum = 1;
+
+   var rightValue= n;
+   var rightSum = n;
+
+   while(leftValue <  rightValue){
+     if(leftSum < rightSum){
+        leftValue++;
+        leftSum += leftValue;
+     }
+     else{
+        rightValue--;
+        rightSum += rightValue;
+     }
+
+     if(leftSum === rightSum && leftValue + 1 === rightValue - 1){
+        return leftValue + 1;
+     }
+
+   }
+
+   return -1;
+
 };
