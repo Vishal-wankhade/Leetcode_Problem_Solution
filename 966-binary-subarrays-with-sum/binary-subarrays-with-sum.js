44f8cacd19597e4faac2bb9094292ed9d898fd83
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-15 20:21:19 +0530

    Time: 62 ms (86.34%) | Memory: 56.8 MB (18.01%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
@@ -3,29 +3,49 @@
  * @param {number} goal
  * @return {number}
  */
-var numSubarraysWithSum = function(nums, goal) {
-  var sum = 0;
-    var count = 0;
+var numSubarraysWithSum = function(nums, k) {
+//   var sum = 0;
+//     var count = 0;
 
-    for(var i = 0; i < nums.length; i++){
-        sum = 0;
-        sum += nums[i];
+//     for(var i = 0; i < nums.length; i++){
+//         sum = 0;
+//         sum += nums[i];
 
-        if(sum == goal){
-            count++;
-        }
-        for(var j = i+1; j < nums.length; j++){
+//         if(sum == goal){
+//             count++;
+//         }
+//         for(var j = i+1; j < nums.length; j++){
             
-            sum += nums[j];
+//             sum += nums[j];
          
-            if(sum == goal){
-                count++;
-            }
+//             if(sum == goal){
+//                 count++;
+//             }
           
              
-        }
-    }
+//         }
+//     }
 
+//     return count;
+ var map = new Map();
+    map.set(0,1);
+
+    var count = 0;
+    var removal = 0;
+    var prefixSum = 0;
+
+    for(var i =0; i < nums.length; i++){
+        prefixSum += nums[i]
+
+        removal = prefixSum - k;
+
+        if(map.has(removal)){
+            count += map.get(removal);
+        }
+
+       map.set(prefixSum , (map.get(prefixSum) || 0) + 1);
+
+    }
     return count;
     
 };
