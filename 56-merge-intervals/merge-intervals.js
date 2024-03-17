7b8a5778459b3506483ef551fa70b6cf0670809c
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-17 17:37:44 +0530

    Time: 94 ms (65.54%) | Memory: 58.7 MB (55.40%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(arr) {
+const n = arr.length;
+  
+  // sort the given intervals
+  arr.sort((a, b) => a[0] - b[0]);
+  
+  const ans = [arr[0]];
+  
+  for (let i = 1; i < n; i++) {
+    const last = ans[ans.length - 1];
+    const curr = arr[i];
+    
+    // if the current interval overlaps with the last interval
+    if (curr[0] <= last[1]) {
+      last[1] = Math.max(last[1], curr[1]);
+    }
+    // if the current interval does not overlap with the last interval
+    else {
+      ans.push(curr);
+    }
+  }
+  
+  return ans;
+    
+};
