c276c86e2db800f436329b406e20282b955e97e8
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-23 18:19:59 +0530

    Time: 47 ms (99.13%) | Memory: 52.4 MB (47.99%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var count = 1;
+
+   for(var i =1; i < nums.length; i++){
+    if(nums[i] !== nums[i-1]){
+         nums[count] = nums[i];
+         count++;
+    }
+   }
+   return count;
+};
