8ad1900e0f12802f134bb68b187bd4be35f7ba27
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-03-26 18:38:19 +0530

    Time: 87 ms (34.42%) | Memory: 55.6 MB (82.34%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+
+    var n = nums.length;
+
+    function swap(nums, i , j){
+        [nums[i],nums[j]] = [nums[j],nums[i]];
+    }
+
+    for(var i = 0; i < n; i++){
+        var ele = nums[i];
+      if(ele > 0  && ele !== nums[ele-1] && ele <= n ){
+        swap(nums,i,ele-1);
+        i--;
+      }
+
+    }
+    for(var i = 0; i <n; i++){
+        if(nums[i]!== i+1){
+            return i+1
+        }
+    }
+
+    return n+1;
+    
+};
