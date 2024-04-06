ae0e9dfbeaf85e2fc73aee198eed71a025fa31a2
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-04-07 02:37:35 +0530

    Time: 49 ms (74.83%) | Memory: 48.2 MB (96.28%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    var n = nums.length;
+
+    var left = 0;
+    var right = n-1;
+    var mid = 0;
+
+    
+    while (left <= right){
+
+      mid =  Math.floor((right + left) /2);
+
+      if(nums[mid] == target){
+        return mid;
+      }
+
+      if(nums[left] <= nums[mid] ){
+        if(nums[left] <= target && target <= nums[mid] ){
+         right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+      }
+      else{
+        if(nums[mid] <= target && target <= nums[right] ){
+         left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+      }
+
+    
+    }
+    return -1;
+};
