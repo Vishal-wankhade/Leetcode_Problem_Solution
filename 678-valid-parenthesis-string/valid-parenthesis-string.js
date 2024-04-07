0b5ed516e9b3254c08b3c679a83f9cd2ab7181f2
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-04-07 21:16:42 +0530

    Time: 53 ms (51.99%) | Memory: 48.9 MB (53.43%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function(s) {
+let low = 0, high = 0;
+
+    for (const c of s) {
+        if (c === '(') {
+            low++;
+            high++;
+        } else if (c === ')') {
+            low = Math.max(0, low - 1);
+            high--;
+        } else if (c === '*') {
+            low = Math.max(0, low - 1);
+            high++;
+        }
+        if (high < 0) {
+            return false;
+        }
+    }
+
+    return low === 0;
+    
+};
