b311818cec07733d338d2cc1fb9fa7e03eeb17a1
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-04-18 22:32:48 +0530

    Time: 125 ms (75.15%) | Memory: 58 MB (60.44%) - LeetSync

--- 463-island-perimeter/island-perimeter.js
+++ 463-island-perimeter/island-perimeter.js
@@ -8,34 +8,57 @@ var islandPerimeter = function(grid) {
     var m = grid[0].length;
     var peri = 0;
 
-    function dfs(grid, i, j){
+    // function dfs(grid, i, j){
 
-        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0)
-        {
-            peri++;
-            return;
-        }
+    //     if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0)
+    //     {
+    //         peri++;
+    //         return;
+    //     }
 
-        if(grid[i][j] == -1){
-            return;
-        }
+    //     if(grid[i][j] == -1){
+    //         return;
+    //     }
 
-        grid[i][j] = -1;
+    //     grid[i][j] = -1;
 
-        dfs(grid, i+1,j);
-        dfs(grid, i-1,j);
-        dfs(grid, i,j+1);
-         dfs(grid, i,j-1);
+    //     dfs(grid, i+1,j);
+    //     dfs(grid, i-1,j);
+    //     dfs(grid, i,j+1);
+    //      dfs(grid, i,j-1);
 
-    }
+    // }
+
+    // for(var i= 0; i < n; i++){
+    //     for(var j= 0; j < m; j++){
+    //       if(grid[i][j] == 1){
+    //         dfs(grid,i,j);
+    //         return peri;
+    //       }
+    // }
+    // }
+    // return peri;
 
     for(var i= 0; i < n; i++){
         for(var j= 0; j < m; j++){
-          if(grid[i][j] == 1){
-            dfs(grid,i,j);
-            return peri;
-          }
-    }
+            if(grid[i][j]== 0){
+                continue;
+            }
+
+            if(j+1 >= m || grid[i][j+1] == 0){
+                peri++;
+            }
+             if(j-1 < 0 || grid[i][j-1]== 0){
+                peri++;
+            }
+             if(i+1 >= n || grid[i+1][j]== 0){
+                peri++;
+            }
+             if(i-1 < 0 || grid[i-1][j]== 0){
+                peri++;
+            }
+
+       }
     }
     return peri;
     
