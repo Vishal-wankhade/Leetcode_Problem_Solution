df321c243cafbb7e57d0b5e86fc51403e54f361c
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-04-19 01:29:46 +0530

    Time: 58 ms (54.72%) | Memory: 50.5 MB (39.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,28 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
 
-    var stack = [];
 
-    for(var c of s){
-        if(c === '(' || c === '{' || c === '['){
-            stack.push(c)
-        }else{
-            if(!stack.length ||
+var stack = [];
 
-            ( c === ')'  && stack[stack.length-1] !== '(') ||
-            ( c === ']'  && stack[stack.length-1] !== '[') ||
-            ( c === '}'  && stack[stack.length-1] !== '{') )
-               {
-                    return false;
-               }
-            stack.pop();
-    
-        
-        }
-
-       
+for(var i = 0; i < s.length; i++){
+    if(s[i] == '(' ||  s[i] == '[' || s[i] == '{' ){
+        stack.push(s[i]);
     }
-     return !stack.length;
-};
+    else{
+        if( s[i] == ')' && stack[stack.length - 1] !== '(' ||
+            s[i] == '}' && stack[stack.length - 1] !== '{' ||
+            s[i] == ']' && stack[stack.length - 1] !== '[')
+        {
+            return false;
+           
+        }
+        else{
+            stack.pop();
+        }
+        
+    }
+}
+
+if(stack.length > 0){
+    return false;
+}else{
+    return true;
+}
+
+}
