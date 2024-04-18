0cea20136705603d723b113ad1bd6c5a9da0696a
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-04-19 01:56:58 +0530

    Time: 47 ms (91.07%) | Memory: 50.5 MB (54.11%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -3,19 +3,31 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersection = function(nums1, nums2) {
+var intersection = function (nums1, nums2) {
 
     let set1 = new Set(nums1)
-let intersectionSet = new Set()
+    let intersectionSet = new Set()
 
-for(let i=0;i<nums2.length;i++){
-    if(set1.has(nums2[i])){
-        intersectionSet.add(nums2[i])
+    for (let i = 0; i < nums2.length; i++) {
+        if (set1.has(nums2[i])) {
+            intersectionSet.add(nums2[i])
+        }
     }
-}
 
-return Array.from(intersectionSet)
+    return Array.from(intersectionSet)
+
+    // let result = new Array();
+
+    // var n = nums1.length;
+
+    // for(let i =0; i <n; i++){
+    //     if(nums2.includes(nums1[i]) && !result.includes(nums1[i])){
+    //         result.push(nums1[i]);
+    //     }
+    // }
+
+    // return result;
 
 
-    
+
 };
