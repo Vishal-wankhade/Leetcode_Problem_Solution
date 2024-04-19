b12d00857d5b7c1b89e39b54c52512d67943ac7e
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-04-19 09:14:14 +0530

    Time: 72 ms (91.93%) | Memory: 52.9 MB (85.62%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -4,27 +4,25 @@
  */
 var lengthOfLongestSubstring = function(s) {
 
-       var map =  new Map();
-    var l = 0;
-    var r = 0;
     var max = 0;
-    
+    var l =0;
+    var r= 0;
+    var map = new Map();
+
+
+
     while(r < s.length){
-        
         if(map.has(s[r])){
-            if(map.get(s[r]) >= l){
-                l = map.get(s[r]) + 1;
-                
+           if(map.get(s[r]) >= l){
+            l = map.get(s[r]) + 1;
             }
+            
         }
-        map.set(s[r],r);
-        
-        
+        map.set(s[r],r)
         max = Math.max(max,r-l+1);
         r++;
+
     }
+
     return max;
-
-
-    
 };
