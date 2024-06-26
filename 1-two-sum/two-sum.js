0f969be580e9ac55efdd08e09d3a0ba1d064f5cd
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-06-26 19:57:42 +0530

    Time: 65 ms (56.62%) | Memory: 51.1 MB (36.79%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,19 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-      var map = new Map();
+    var map = new Map();
 
     for(var i = 0; i < nums.length; i++){
-
-        let value = nums[i]
-        let  pair = target-value;
-
-        if(map.has(pair)){
-            return [map.get(pair),i];
-        }
-        else{
-            map.set(value , i)
-        }
+         var value = target - nums[i];
+         if(map.has(value)){
+            return [map.get(value),i];
+         }else{
+            map.set(nums[i],i);
+         }
     }
-    
+    return -1;
 };
