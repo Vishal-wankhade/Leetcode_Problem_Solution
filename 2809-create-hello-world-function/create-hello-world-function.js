7ad9c2173cb890376d7ce48a6088d1c4b7fe3334
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-06-26 20:11:57 +0530

    Time: 45 ms (84.76%) | Memory: 48.8 MB (22.03%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+     
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+createHelloWorld();
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
