8b8d44c28112b607475d5074e842ee0781f8470b
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-06-28 00:40:56 +0530

    Time: 179 ms (6.48%) | Memory: 58.5 MB (30.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var str= x.toString();
+    console.log(str)
+    var i = 0;
+    var j = str.length-1;
+
+
+    while( i < j){
+        if(str[i]!==str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+};
