93c8410f976680ea473be353f64b5cca98144d24
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-06-28 01:54:37 +0530

    Time: 97 ms (11.61%) | Memory: 60.9 MB (14.85%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -3,27 +3,26 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
 
-
-    function rev(nums,start,end){
-        while(start<end){
-            [nums[start],nums[end]] = [nums[end],nums[start]]
-            start++;
-            end--;
-        }
+ function swap(i,j,nums){
+    while(i<j){
+        [nums[i],nums[j]] = [nums[j],nums[i]];
+        i++;
+        j--;
     }
-var n = nums.length;
-  var k = k % n;
-
-  nums = nums.reverse();
-
-  rev(nums,0, k-1);
-  rev(nums,k, n-1);
-
-
-
-
-
+    return nums;
+ }
+var rotate = function(nums, k) {
+    var n = nums.length;
+     var k = k % n;
+    if(nums.length ==1){
+        return;
+    }
     
+    nums = nums.reverse();
+
+    swap(0,k-1,nums);
+    swap(k,nums.length-1,nums)
+
+
 };
