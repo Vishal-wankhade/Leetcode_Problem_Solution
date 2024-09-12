7ed2505b877672844ee72b41f97575dbe7a447af
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-09-13 01:00:20 +0530

    Time: 52 ms (83.67%) | Memory: 51.7 MB (28.89%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -2,10 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) { 
-      const n = nums.length;
-    const Tsum = (n * (n + 1)) / 2;
-    const actualSum = nums.reduce((acc, num) => acc + num, 0);
-    return Tsum - actualSum;
+var missingNumber = function(nums) {
     
+ var n = nums.length;
+ var totalSum = (n*(n+1))/2;
+
+ var sum = 0;
+
+ for(var i= 0; i< n; i++){
+    sum += nums[i];
+ }
+
+ return totalSum - sum;
 };
