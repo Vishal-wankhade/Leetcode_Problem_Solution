dd62999f99123a8ad3dac9cd987ced0bea7e15b6
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-09-15 01:13:06 +0530

    Time: 58 ms (29.06%) | Memory: 48.8 MB (79.08%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,16 +3,33 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-   let count = [0, 0, 0];
-    for (let num of nums) {
-        count[num]++;
-    }
     
-    let i = 0;
-    for (let color = 0; color < count.length; color++) {
-        for (let j = 0; j < count[color]; j++) {
-            nums[i] = color;
-            i++;
-        }
-    } 
-};
+var mid = 0;
+var start = 0;
+var end = nums.length-1;
+var temp = 0;
+
+
+while(mid <= end)
+{
+ if(nums[mid] == 0){
+    temp = nums[start];
+   nums[start] = nums[mid];
+   nums[mid] = temp;
+   start++;
+   mid++;
+ }else if(nums[mid] == 2){
+    
+    temp = nums[end];
+   nums[end] = nums[mid];
+   nums[mid] = temp;
+   end--;
+ }
+ else{
+   
+   mid++;
+ }
+
+
+}
+}
