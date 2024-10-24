3bc24e0e14b39443363f3e96ef5a7d6098b90b2f
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-10-25 00:56:13 +0530

    Time: 46 ms (94.07%) | Memory: 65.7 MB (47.31%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,36 +3,39 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
- var ans = [];
-    nums = nums.sort((a, b) => a - b); // Sort array
-    var target = 0;
-    var n = nums.length;
 
-    for (var i = 0; i < n - 2; i++) {
-        // Skip duplicates for 'i'
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
 
-        var left = i + 1;
-        var right = n - 1;
+var ans = [];
+ nums = nums.sort((a,b) => a - b)
+var target = 0;
+var n = nums.length;
 
-        while (left < right) {
-            var sum = nums[i] + nums[left] + nums[right];
-            if (sum === target) {
-                ans.push([nums[i], nums[left], nums[right]]); // Push the actual triplet
 
-                // Skip duplicates for 'left' and 'right'
-                while (left < right && nums[left] === nums[left + 1]) left++;
-                while (left < right && nums[right] === nums[right - 1]) right--;
 
-                left++;
-                right--;
-            } else if (sum > target) {
-                right--;
-            } else {
-                left++;
-            }
-        }
+for(var i = 0; i < n - 2; i++){
+    var left = i + 1;
+    var right = n - 1;
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    while(left < right){
+      var sum = nums[i] + nums[left] + nums[right];
+      if(sum === target){
+        ans.push([nums[i] ,nums[left], nums[right]]);
+
+      while(nums[left] === nums[left + 1]) left++;
+      while(nums[right] === nums[right - 1]) right--;
+
+      left++;
+      right--;
+
+      }else if(sum > target){
+        right--;
+      }else{
+        left++;
+      }
     }
+}
 
-    return ans;
+   return ans; 
 };
