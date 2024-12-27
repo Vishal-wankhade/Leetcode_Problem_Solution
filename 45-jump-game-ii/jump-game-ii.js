a15660ab9b728d7f54f7241d40d2c99f1994b8f4
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2024-12-27 12:06:25 -0800

    Time: 2 ms (59.15%) | Memory: 49.5 MB (99.80%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -3,28 +3,17 @@
  * @return {number}
  */
 var jump = function(nums) {
-    var n = nums.length;
+      let near = 0, far = 0, jumps = 0;
 
-    var lastIdx = 0;
-    var coverage = 0;
-    var jumps = 0;
-    var destination = n-1;
-
-    if(n==1)return 0;
-
-    for(var i= 0; i <n; i++){
-
-        coverage = Math.max(coverage, i + nums[i]);
-
-        if(i == lastIdx){
-            lastIdx = coverage;
-            jumps++; 
-
-          if(coverage >= destination){
-            return jumps;
-            }
+    while (far < nums.length - 1) {
+        let farthest = 0;
+        for (let i = near; i <= far; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
         }
-
+        near = far + 1;
+        far = farthest;
+        jumps++;
     }
-    return jumps
+
+    return jumps;    
 };
