de844c9747d88258b84b0d92cbb2a0e3dc8a2133
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-01-07 00:37:01 +0530

    Time: 3 ms (26.97%) | Memory: 49.2 MB (56.59%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+let i = 0;
+var j = 0;
+
+while(j < t.length){
+
+    if(s[i] === t[j]){
+        i++;
+    }
+    j++;
+}
+
+return i == s.length ? true : false;
+
+};
