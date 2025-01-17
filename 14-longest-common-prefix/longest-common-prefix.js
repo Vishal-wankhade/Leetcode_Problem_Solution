f6d9586741e185d2876300aceff3736dd8aa81b5
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-01-17 17:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 49 MB (88.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,26 +3,19 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-
-    var ans = "";
-    strs.sort()
     
+  strs.sort();
 
-    var first = strs[0];
-    var last = strs[strs.length-1];
+  let first = strs[0];
+  let last = strs[strs.length - 1];
+  
+  var ans = '';
 
-    for(var i = 0; i < Math.min(first.length,last.length); i++){
-        if(first[i] !== last[i])
-        {
-            return ans;
-        }
-        ans += first[i];
+  for(let i = 0; i < Math.min(first.length, last.length); i++){
+    if(first[i]  !== last[i]){
+        return ans;
     }
-
-
-    return ans;
-   
-
-
-    
+    ans += first[i];
+  }
+  return ans;
 };
