e4db35a7929beef4d30879954a761854733c433f
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-03-11 15:59:59 +0530

    Time: 196 ms (5.08%) | Memory: 63.9 MB (5.21%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,9 +4,28 @@
  */
 var isPalindrome = function(s) {
     
-    var str = s.replace(/[^a-z^A-Z0-9+]/g, '').toLowerCase().split("").reverse().join("");
+    // var str = s.replace(/[^a-z^A-Z0-9+]/g, '').toLowerCase().split("").reverse().join("");
     
 
-    return str == s.replace(/[^a-z^A-Z0-9+]/g, '').toLowerCase();
+    // return str == s.replace(/[^a-z^A-Z0-9+]/g, '').toLowerCase();
+
+     var str = s.replace(/[^a-z^A-Z0-9+]/g, '').toLowerCase();
+
+   var start = 0;
+   var end = str.length-1;
+
+   while(start <= end){
+    console.log(str[start])
+    console.log(str[end])
+
+    if(str[start] != str[end]){
+    
+        return false;
+    }
+    start++;
+    end--;
+   }
+   return true;
+
     
 };
