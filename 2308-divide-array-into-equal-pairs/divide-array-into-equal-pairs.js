34fe73f0b32419f9478dd2c8fe348d00b378e2cc
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-03-17 14:35:21 +0530

    Time: 4 ms (70.97%) | Memory: 55.6 MB (28.57%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
@@ -4,21 +4,16 @@
  */
 var divideArray = function(nums) {
 
-    let map = new Map();
+    let set = new Set();
 
     for(let ele of nums){
-       map.set(ele, (map.get(ele) || 0) + 1 )
+       if(set.has(ele)){
+        set.delete(ele);
+       }else{
+        set.add(ele)
+       }
     }
 
-   let sum =0;
-    for(let [key , value] of map.entries()){
-        sum += value;
-        if((value % 2 !== 0) || sum % 2 !== 0){
-            return false;
-        }
-    }
-    return true;
-
-    console.log(map)
-    
+  
+    return set.size == 0;
 };
