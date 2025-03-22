585e174e9db32238548b0834714ca85397ed3669
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-03-22 17:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 58.7 MB (6.37%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,41 +3,24 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    
- if (nums.length === 0) return 0; // edge case: empty array
-    
-    let i = 1; // pointer for placing the next unique element
-    
-    for (let j = 1; j < nums.length; j++) {
-        if (nums[j] !== nums[j - 1]) {
-            nums[i] = nums[j];
-            i++;
-        }
+    let pre = nums[0];
+
+    let count = 1;
+
+    for(let i = 1; i < nums.length; i++){
+
+      if(pre != nums[i]){
+      nums[count] = nums[i];
+      count++;
+      pre = nums[i]
     }
-    
-    return i;
+    }
 
-
-//      var last = nums[i];
-
-// var i = 0;
-// var j = 1;
-
-// while(j < nums.length){
-//     if(nums[j] === last){
-//         j++;
-//     }
-//     else{
-//         last = nums[j];
-//         [nums[i],nums[j]]=[nums[j],nums[i]]
-//         i++;
-//         j++;
-        
-//     }
-// }
-// return nums;
-  
-
-
-
+    // if(count != nums.length){
+    //     while(count < nums.length){
+    //         nums[count] = 0;
+    //         count++;
+    //     }
+    // }
+    return count;
 };
