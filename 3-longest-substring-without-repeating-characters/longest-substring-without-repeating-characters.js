4b905fa6c23f3028fef68098077ca755a3975397
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-03-28 16:37:57 +0530

    Time: 3 ms (96.99%) | Memory: 55.9 MB (70.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,25 +2,26 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    
-    if(s == "") return 0;
+var lengthOfLongestSubstring = function (s) {
+
     let map = new Map();
-    let max = -Infinity;
+
 
     let left = 0;
     let right = 0;
+    let max = 0;
 
-    while(right < s.length){
-        if(map.has(s[right])){
-            if(left <= map.get(s[right])){
-               left = map.get(s[right]) + 1;
-               map.set(s[right],right);
+    while (right < s.length) {
+        if (map.has(s[right])) {
+            if (left <= map.get(s[right])) {
+                left = map.get(s[right]) + 1;
             }
         }
-           map.set(s[right],right);
-           max = Math.max(max, (right-left)+1)
-           right++;
+
+        map.set(s[right], right);
+
+        max = Math.max(max, right - left + 1);
+        right++;
     }
-  return max;  
+    return max;
 };
