c4a2c622ec10b6677a1c30c3502990c958615345
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2025-12-09 01:01:54 +0530

    Time: 67 ms (8.48%) | Memory: 54.9 MB (92.27%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    function helper(x, rev) {
+        if (x === 0) return rev;
+        if (rev > Math.trunc(2147483647 / 10) || rev < Math.trunc(-2147483648 / 10)) return 0;
+
+        let rem = x % 10;
+        x = Math.trunc(x / 10);
+        rev = rev * 10 + rem;
+
+        return helper(x, rev);
+    }
+
+    return helper(x, 0);
+};
